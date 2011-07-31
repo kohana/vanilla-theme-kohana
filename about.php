1cd535eeacb96b0d7321a4a1a4f65c61736e71bd
Kiall Mac Innes <kiall@managedit.ie>
2011-07-31 18:33:12 +0100

    Initial commit

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php if (!defined('APPLICATION')) exit();
+/**
+ * An associative array of information about this application.
+ */
+$ThemeInfo['kohana'] = array(
+   'Name' => 'Vanilla Kohana',
+   'Description' => "The Kohana theme for Vanilla.",
+   'Version' => '0.1',
+   'Author' => "Kohana Team",
+   'AuthorEmail' => 'alwayson@kohanaframework.org',
+   'AuthorUrl' => 'http://kohanaframework.org/',
+);
